0a38a2b88beaf312d29bf8b89fa1cabc4b918656
Rishu1010 <rishupandeypandit@gmail.com>
2025-09-16 17:34:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+def get_cleaned_data(form_data):
+    gestation = float(form_data['gestation'])
+    parity = int(form_data['parity'])
+    weight = float(form_data['weight'])
+    age = float(form_data['age'])
+    height = float(form_data['height'])
+    smoke = float(form_data['smoke'])
+
+    cleaned_data = {
+        "gestation": gestation,
+        "parity": parity,
+        "weight": weight,
+        "age": age,
+        "height": height,
+        "smoke": smoke
+    }
+    return cleaned_data
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    # Support both JSON (API) and form (HTML) submissions
+    if request.is_json:
+        baby_data = request.get_json()
+    else:
+        baby_data = request.form
+
+    baby_data_cleaned = get_cleaned_data(baby_data)
+    baby_df = pd.DataFrame([baby_data_cleaned])
+
+    with open('model/model.pkl', 'rb') as obj:
+        model = pickle.load(obj)
+
+    prediction = model.predict(baby_df)
+    prediction = round(float(prediction[0]), 2)
+
+    response = {"Prediction": prediction}
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+   
